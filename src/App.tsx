9e4f45469fec61e8c33a356456c68bf1c33d283e
TEST <bob@example.com>
2020-11-21 23:11:41 +0800

    tone loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import useApi from "./useApi";
 import * as Tone from "tone";
-
+import fakeData from "./fakeApi.json";
 function App() {
   const { data } = useApi();
+  const [toneInitialized, setToneInitialized] = useState(false);
   const synth = new Tone.Synth().toDestination();
+  const bassSynth = new Tone.MembraneSynth().toDestination();
 
   const play = async () => {
-    await Tone.start();
-    console.log("audio is ready");
-    const now = Tone.now();
-    synth.triggerAttackRelease("C4", "8n", now);
-    synth.triggerAttackRelease("E4", "8n", now + 0.5);
-    synth.triggerAttackRelease("G4", "8n", now + 1);
+    if (!toneInitialized) {
+      await Tone.start();
+      console.log("audio is ready");
+      setToneInitialized(true);
+    }
+
+    const loop = new Tone.Loop((time) => {
+      synth.triggerAttackRelease("G2", "4n", time);
+      bassSynth.triggerAttackRelease("c0", "16n", time);
+    }, "4n").start(0);
+    Tone.Transport.bpm.value = 128;
+    Tone.Transport.start();
+    // const now = Tone.now();
+    // synth.triggerAttackRelease("C4", "8n", now);
+    // synth.triggerAttackRelease("E4", "8n", now + 0.5);
+    // synth.triggerAttackRelease("G4", "8n", now + 1);
+  };
+
+  const stop = async () => {
+    Tone.Transport.stop();
   };
 
   return (
     <div>
       <div>
-        {" "}
         <button onClick={play}>Start Sound</button>
+        <button onClick={stop}>Stop Sound</button>
       </div>
-      <div>{JSON.stringify(data)}</div>
+      <div>{JSON.stringify(data ?? fakeData)}</div>
     </div>
   );
 }
--- src/useApi.ts
+++ src/useApi.ts
@@ -2,15 +2,14 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function useApi() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState<any>();
   const getData = async () => {
     try {
       const response = await axios.get(
-        "https://cors-anywhere.herokuapp.com/https://covid-api.mmediagroup.fr/v1/cases",
-        { httpAgent: true }
+        "https://cors-anywhere.herokuapp.com/https://covid-api.mmediagroup.fr/v1/cases"
       );
       setData(response);
-      console.log(response);
+      console.log("Api response", response);
     } catch (err) {
       console.error(err);
     }
