3bf48cca3a78d10e6d15bc1f6242814027568298
TEST <bob@example.com>
2020-11-21 20:30:30 +0800

    sound

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import useApi from "./useApi";
+import * as Tone from "tone";
 
 function App() {
   const { data } = useApi();
-  return <div>{JSON.stringify(data)}</div>;
+  const synth = new Tone.Synth().toDestination();
+
+  const play = async () => {
+    await Tone.start();
+    console.log("audio is ready");
+    const now = Tone.now();
+    synth.triggerAttackRelease("C4", "8n", now);
+    synth.triggerAttackRelease("E4", "8n", now + 0.5);
+    synth.triggerAttackRelease("G4", "8n", now + 1);
+  };
+
+  return (
+    <div>
+      <div>
+        {" "}
+        <button onClick={play}>Start Sound</button>
+      </div>
+      <div>{JSON.stringify(data)}</div>
+    </div>
+  );
 }
 
 export default App;
